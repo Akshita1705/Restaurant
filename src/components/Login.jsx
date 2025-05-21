2d6ef1894b4650077b463f0fe76af8b7b67f81ba
Akshita1705 <akshitainvizag@gmail.com>
2025-05-21 22:13:27 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,17 +10,15 @@ import {
     IconButton,
     Divider
 } from '@mui/material';
+
 import CloseIcon from '@mui/icons-material/Close';
 import EmailIcon from '@mui/icons-material/Email';
-import GoogleIcon from '@mui/icons-material/Google';
 import '../css/Login.css';
-
 
 const Login = ({ switchToSignup, switchToEmail }) => {
     const navigate = useNavigate();
-
     const [phoneNumber, setPhoneNumber] = useState('');
-  
+
     return (
         <Container maxWidth="sm" className="login-container">
             <Paper elevation={3} className="login-paper">
@@ -29,86 +27,65 @@ const Login = ({ switchToSignup, switchToEmail }) => {
                         Login
                     </Typography>
                     <IconButton aria-label="close" onClick={() => navigate(-1)}>
-    <CloseIcon />
-</IconButton>
-
+                        <CloseIcon />
+                    </IconButton>
                 </Box>
 
-                <Box component="form" >
-    <Box className="phone-number" >
-       <Box className="flag">
-  <img
-    src="https://flagcdn.com/w40/in.png"
-    alt="India Flag"
-   
-  />
-</Box>
+                <Box component="form" className="phone-form">
+                    <Box className="phone-input-wrapper">
+                        <Box className="country-selector">
+                            <img
+                                src="https://flagcdn.com/w40/in.png"
+                                alt="India Flag"
+                                className="flag-img"
+                            />
+                            <Typography className="country-code">+91</Typography>
+                        </Box>
 
+                        <TextField
+                            placeholder="Phone"
+                            variant="outlined"
+                            size="small"
+                            value={phoneNumber}
+                            onChange={(e) => setPhoneNumber(e.target.value)}
+                            fullWidth
+                            className="phone-input"
+                        />
+                    </Box>
 
+                    <Button type="submit" fullWidth variant="contained" className="submit-button">
+                        Send One Time Password
+                    </Button>
 
-        <TextField
-            value="+91"
-            size="small"
-            variant="outlined"
-            disabled
-            className='prefix'
-         
-        />
+                    <Box className="divider-box">
+                        <Divider className="divider-line" />
+                        <Typography variant="body2" className="divider-text">or</Typography>
+                        <Divider className="divider-line" />
+                    </Box>
 
-        <TextField
-            placeholder="Enter your phone number"
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
-            size="small"
-            fullWidth
-            variant="outlined"
-        />
-    </Box>
+                    <Button
+                        fullWidth
+                        variant="outlined"
+                        startIcon={<EmailIcon />}
+                        onClick={switchToEmail}
+                        className="email-button"
+                    >
+                        Continue with Email
+                    </Button>
 
-    <Button type="submit" fullWidth variant="contained" className="submit-button">
-        Send One time password
-    </Button>
-
-    <Box className="divider-box">
-        <Divider className="divider-line" />
-        <Typography variant="body2" className="divider-text">or</Typography>
-        <Divider className="divider-line" />
-    </Box>
-
-    <Button
-        fullWidth
-        variant="contained"
-        startIcon={<EmailIcon />}
-       onClick={switchToEmail}
-        className="submit-button"
-    >
-        Continue with Email
-    </Button>
-
-    <Button
-        fullWidth
-        variant="contained"
-        startIcon={<GoogleIcon />}
-        className="submit-button"
-    >
-        Continue with Gmail
-    </Button>
-
-    <Box className="form-footer">
-        <Typography variant="body2" className="footer-text">
-            New to our APP ?
-        </Typography>
-        <Typography
-            variant="body2"
-            className="footer-link"
-             onClick={switchToSignup}
-           
-        >
-            Create Account
-        </Typography>
-    </Box>
-</Box>
-
+                    <Box className="form-footer">
+                        <Typography variant="body2" className="footer-text">
+                            New?
+                        </Typography>
+                        <Typography
+                            variant="body2"
+                            className="footer-link"
+                            onClick={switchToSignup}
+                        >
+                            Create account
+                        </Typography>
+                    </Box>
+                </Box>
             </Paper>
         </Container>
     );
